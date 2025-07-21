679d7e517effe4d698e9acd8aff66cc16aa66231
daikimaeda420 <daiki.maeda.web@gmail.com>
2025-07-21 16:58:18 +0900

    Update layout.tsx

--- app/(with-header)/layout.tsx
+++ app/(with-header)/layout.tsx
@@ -2,6 +2,7 @@
 "use client";
 
 import Header from "@/components/Header";
+import { SessionProvider } from "next-auth/react";
 
 export default function WithHeaderLayout({
   children,
@@ -9,9 +10,9 @@ export default function WithHeaderLayout({
   children: React.ReactNode;
 }) {
   return (
-    <>
+    <SessionProvider>
       <Header />
       {children}
-    </>
+    </SessionProvider>
   );
 }
