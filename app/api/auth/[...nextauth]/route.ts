b9b8db6bb32c8fe836336282ab6a2a64359db0b7
daikimaeda420 <daiki.maeda.web@gmail.com>
2025-07-21 16:35:10 +0900

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,9 +2,6 @@
 import NextAuth from "next-auth";
 import { authOptions } from "@/lib/authOptions";
 
-const handler = NextAuth({
-  ...authOptions,
-  trustHost: true, // ← ここが絶対に必要！
-});
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
