2b4d8695aab7ce16c0d3900cc48ed69402bf28f3
daikimaeda420 <daiki.maeda.web@gmail.com>
2025-07-23 13:00:02 +0900

    ｙ

--- middleware.ts
+++ middleware.ts
@@ -1,40 +1,26 @@
 // middleware.ts
-// import { withAuth } from "next-auth/middleware";
-
-// export default withAuth({
-//   pages: { signIn: "/login" },
-//   callbacks: {
-//     authorized({ token, req }) {
-//       console.log(
-//         "[middleware]",
-//         "checking auth - path=",
-//         req.nextUrl.pathname
-//       );
-//       console.log("[middleware]", "token=", token);
-//       return !!token;
-//     },
-//   },
-// });
-
-// export const config = {
-//   matcher: [
-//     "/after-login",
-//     "/schools/:path*",
-//     "/superadmin/:path*",
-//     "/admin/:path*",
-//   ],
-// };
 import { withAuth } from "next-auth/middleware";
 
 export default withAuth({
   pages: { signIn: "/login" },
   callbacks: {
     authorized({ token, req }) {
-      if (!token) {
-        req.headers.set("x-token-status", "no-token");
-        return false;
-      }
-      return true;
+      console.log(
+        "[middleware]",
+        "checking auth - path=",
+        req.nextUrl.pathname
+      );
+      console.log("[middleware]", "token=", token);
+      return !!token;
     },
   },
 });
+
+export const config = {
+  matcher: [
+    "/after-login",
+    "/schools/:path*",
+    "/superadmin/:path*",
+    "/admin/:path*",
+  ],
+};
