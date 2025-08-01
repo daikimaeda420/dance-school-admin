1f83539f38c3f20db1d6876b60856dbfceefc0a4
daikimaeda420 <daiki.maeda.web@gmail.com>
2025-08-01 13:02:52 +0900

    4

--- app/api/check-super-admin/route.ts
+++ app/api/check-super-admin/route.ts
@@ -1,7 +1,11 @@
+import { getServerSession } from "next-auth"; // ← 追加
+import { authOptions } from "@/lib/authOptions";
+
 export async function GET() {
   const session = await getServerSession(authOptions);
-  const roles = session?.user?.roles ?? [];
+  const role = session?.user?.role;
 
-  const isServiceAdmin = roles.includes("service-admin");
+  const isServiceAdmin = role === "service-admin";
+
   return new Response(JSON.stringify({ ok: isServiceAdmin }), { status: 200 });
 }
